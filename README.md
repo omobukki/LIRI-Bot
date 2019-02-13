3f5fdd3cc8338b4a922f1712a788abbbf4850e43
omobukki <42176174+omobukki@users.noreply.github.com>
2019-02-12 23:47:08 -0600

    Liri-Bot

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # LIRI-Bot
+
+
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+What Each Command Should Do
+spotify-this-song- is created for customer or manager to know how this app works. Spotify play like and the  identified song, you can click a button and it will open it in Spotify, or it will add it to a playlist in Spotify so you can listen to it later.
+
+This will show the following information about the song in your terminal/bash window:
+Artist(s)
+The song's name
+A preview link of the song from Spotify
+Songs
+Albums
+Artists
+Playlists
+Podcast shows and episodes 
+Videos
+
+This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+
+Name of the venue
+
+Venue location
+
+Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+node liri.js spotify-this-song '<song name here>'
+
+
+If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
+
+The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
+
+This will output the following information to your terminal/bash window:
+
+  * Title of the movie.
+  * Year the movie came out.
+  * IMDB Rating of the movie.
+  * Rotten Tomatoes Rating of the movie.
+  * Country where the movie was produced.
+  * Language of the movie.
+  * Plot of the movie.
+  * Actors in the movie.
+If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
---
+++ liri.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var moment = require('moment');
+var keys = require("./key.js");
+var spotify = new Spotify(keys.spotify);
+
+//run this in console to test
+//node liri.js concert-this
+var command = process.argv[2];
+var details = process.argv[3];
+
+liri();
+
+function liri() {
+    switch (command) {
+        case "concert-this":
+            //logic for concert-this command
+            var url = "https://rest.bandsintown.com/artists/" + details + "/events?app_id=codingbootcamp";
+
+            axios.get(url)
+                .then(function (response) {
+                    response.data.forEach(event => {
+                        var venueName = event.venue.name;
+                        var city = event.venue.city;
+                        var state = event.venue.region;
+                        var date = moment(event.datetime).format("MM/DD/YYYY")
+                        console.log("Venue Name: " + venueName);
+                        console.log("Location: " + city + ", " + state);
+                        console.log("Date: " + date);
+                    });
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            break;
+        case "spotify-this-song":
+            spotify.search({ type: 'track', query: details, limit: 1 }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                var artist = data.tracks.items[0].album.artists[0].name;
+                var album = data.tracks.items[0].album.name;
+                var previewUrl = data.tracks.items[0].preview_url;
+                console.log("Artist: "+ artist);
+                console.log("Song: " + details);
+                console.log("Album: " + album);
+                console.log("Preview: " + previewUrl);
+            });
+            break;
+        case "movie-this":
+            var url = "http://www.omdbapi.com/?t="+ details +"&apikey=trilogy";
+            axios.get(url)
+            .then(function(response) {
+                var title = response.data.Title;
+                console.log("Title: " + title);
+                var year = response.data.Year;
+                console.log("Year: " + year );
+                // var IMDB = response.data.IMDB;
+                console.log("IMDB: " + IMDB);
+                var rotten = response.data.Rotten;
+                console.log("rotten:" + Rotten);
+                var country = response.data.country;
+                console.log("country: " + Country);
+                var language = response.data.Language;
+                console.log("language: " + language);
+                var plot = response.data.Plot;
+                console.log("plot: " + plot);
+                var actors = response.data.Actors;
+                console.log("actors: "+ Actors);
+                
+                
+                
+                
+                // * Language of the movie.
+                // * Plot of the movie.
+                //* Actors in the movie.
+                
+                //console.log(response.data);
+                //console.log("The movie's rating is: " + response.data.imdbRating);
+            });
+            break;
+        case "do-what-it-says":
+            break;
+        default:
+            console.log("That is not a valid command")
+    }
+}
